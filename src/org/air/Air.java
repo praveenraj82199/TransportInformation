a3792a0a8d26becb72a49ddc314f5a6674c0a9fc
praveenraj82199 <rajpraveen648@gmail.com>
2024-06-17 12:23:51 +0530

    new master to p1 and p2

---
+++ src/org/air/Air.java
@@ -0,0 +1,15 @@
+package org.air;
+
+public class Air {
+	public static void main(String[]args) {
+		int n= 100;
+		System.out.println("list of even number from 1to100:\n");
+		for(int i=1; i<=n; i++) {
+			if(i%2==0) {
+				System.out.println(i+"");
+			}
+		}
+	}
+		
+	}
+	
